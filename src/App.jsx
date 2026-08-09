3dbe55a929abf6550c587e0914b27d7cc94cecfd
Niket-Aggarwal <21.xiia.niketaggarwal@gmail.com>
2026-08-09 11:56:51 +0530

    Starting Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-white bg-black'>Starting...</h1>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes> */}
+    </>
+  )
+}
+
+export default App
